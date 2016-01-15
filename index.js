09250719f0a7634d74bfa8a1140f04ffe2d8e333
Marian Hello <marian.hello@gmail.com>
2016-01-15 01:00:20 +0100

    fixing #37 module exports

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = require('./lib/RefreshableListView')
+'use strict';
+
+// Export RefreshableListView
+var RefreshableListView = {
+  get DataSource() {
+    return require('./lib/ListView').DataSource;
+  },
+  get RefreshingIndicator() {
+    return require('./lib/RefreshingIndicator');
+  },
+  get ControlledRefreshableListView() {
+    return require('./lib/ControlledRefreshableListView');
+  },
+
+  ...require('./lib/RefreshableListView'),
+};
+
+module.exports = RefreshableListView;
